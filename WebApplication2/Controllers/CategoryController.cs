7889f2147cb67bb88a5dd83dce42b7c0e5cf1d77
alrieq <alrieq@live.com>
2023-09-06 11:40:42 +0300

    Add Category Controller

--- WebApplication2/Controllers/CategoryController.cs
+++ WebApplication2/Controllers/CategoryController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
         {
             return View();
         }
